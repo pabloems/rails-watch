708ccc0165a42ac58df54e7d11a761f48dce5421
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-04 16:44:28 -0400

    Se creó controlador de list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  before_action :set_list, only[:show]
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = @list.new(list_params)
+
+    if @list.save
+      redirect_to @lists, notice: "La lista fue creada"
+    else
+      render :new
+    end
+  end
+
+private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
