8526c978727c657ffc0a68a28cbeb6b4f767d8b5
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-06 17:17:53 -0400

    se realizó la vista del show y se afinaron detalles de vistas

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,7 @@ const initUpdateNavbarOnScroll = () => {
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
+        navbar.classList.add('navbar-lewagon-black');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
       }
